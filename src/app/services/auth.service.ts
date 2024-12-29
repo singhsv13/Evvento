77e31949695e3c5e1ff038267c24aca82cfdc268
SV <simarprit.virdi13@gmail.com>
2024-12-29 11:46:19 +0530

    updates

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,65 +17,6 @@ export class AuthService {
     private dialogService: DialogueService
   ) {}
 
-  //   logIn(email: string, password: string): Observable<User | null> {
-  //     return this.userService.authenticateUser(email, password).pipe(
-  //       tap((user) => {
-  //         if (user) {
-  //           this.loggedUser = user;
-  //           this.dialogService.showDialogue('loginSuccess');
-  //           console.log('Login successful:', user);
-  //         } else {
-  //           this.dialogService.showDialogue('loginFailure')
-  //           console.log('Login failed: Invalid credentials');
-  //         }
-  //       })
-  //     );
-  //   }
-
-  //   registerUser(name: string, email: string, password: string): Observable<User | null> {
-  //     return this.userService.getAllUsers().pipe(
-  //       map((users) => users.find((user) => user.email === email)),
-  //       switchMap((existingUser) => {
-  //         if (existingUser) {
-  //           this.dialogService.showDialogue('registrationError');
-  //           return of(null);
-  //         } else {
-  //           const newUser = new User(name, email, password);
-  //           return this.userService.addUser(newUser).pipe(
-  //             map(() => {
-  //               this.loggedUser = newUser;
-  //               this.dialogService.showDialogue('registrationSuccess');
-  //               console.log('Registration successful:', newUser);
-  //               return newUser;
-  //             })
-  //           );
-  //         }
-  //       })
-  //     );
-  //   }
-
-  //   getUserId(): string | null {
-  //     return this.loggedUser?.id || null;
-  //   }
-
-  //   getActiveUser(): User | null {
-  //     return this.loggedUser;
-  //   }
-
-  //   logOut(): void {
-  //     if (this.loggedUser) {
-  //       this.dialogService.showDialogue('logoutConfirmation');
-  //       console.log(`${this.loggedUser.name} logged out.`);
-  //     }
-  //     this.loggedUser = null;
-  //     this.router.navigate(['/login']);
-  //   }
-
-  //   isAuthenticated(): boolean {
-  //     return !!this.loggedUser;
-  //   }
-  // }
-
   logIn(email: string, password: string): Observable<User | null> {
     return this.userService.authenticateUser(email, password).pipe(
       tap((user) => {
