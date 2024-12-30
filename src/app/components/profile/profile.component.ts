92916ab8c9049bfe85c209291e10a4f9d066b6e2
SV <simarprit.virdi13@gmail.com>
2024-12-30 20:04:03 +0530

    added delete user fucntionality

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { EventService } from 'src/app/services/event.service';
 import { DialogueService } from 'src/app/services/dialogue.service'; // Import the DialogueService
 import { forkJoin } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,12 +12,14 @@ import { forkJoin } from 'rxjs';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+
   user: User;
 
   constructor(
     private authService: AuthService,
     private eventService: EventService,
-    private dialogueService: DialogueService // Inject the DialogueService
+    private dialogueService: DialogueService, // Inject the DialogueService
+    private userService : UserService
   ) {}
 
   ngOnInit(): void {
@@ -43,9 +46,43 @@ export class ProfileComponent implements OnInit {
     });
   }
 
+  deleteUserProfile(): void {
+    if (this.user) {
+      this.userService.deleteUser(this.user.id).subscribe({
+        next: () => {
+          this.dialogueService.showDialogue('userDeleted');
+          setTimeout(()=>{
+            this.authService.logOut(); 
+          },2000)
+        },
+        error: (error) => {
+          console.error('Error deleting user:', error);
+          this.dialogueService.showDialogue('serverError');
+        }
+      });
+    }
+  }
+
+  editUserDetails(): void {
+    // if (this.user) {
+    //   // Assuming you have a form to edit user details.
+    //   this.userService.updateUser(this.user.id, this.user).subscribe({
+    //     next: () => {
+    //       this.dialogueService.showDialogue('userUpdated');
+    //     },
+    //     error: (error) => {
+    //       console.error('Error updating user:', error);
+    //       this.dialogueService.showDialogue('serverError');
+    //     }
+    //   });
+    // }
+  }
+
+
   logout(): void {
     this.authService.logOut();
     // Show the logout confirmation dialogue after logging out
     this.dialogueService.showDialogue('logoutConfirmation');
   }
+
 }
